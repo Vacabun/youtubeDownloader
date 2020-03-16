32987a94c147d2d7e16fcbe4cb2358af8727f6e3
Vacabun <maguotong66@gmail.com>
2020-03-16 21:17:53 +0800

    基本实现功能

--- src/main.py
+++ src/main.py
@@ -1,13 +1,15 @@
-import sys,os,subprocess,threading,re
-from PyQt5.QtWidgets import (QDesktopWidget,QWidget, QPushButton, QApplication,QHBoxLayout, QVBoxLayout, QLabel, QInputDialog,QFileDialog,QMessageBox,QProgressDialog)
+import sys, os, subprocess, threading, re
+from PyQt5.QtWidgets import (QDesktopWidget, QWidget, QPushButton, QApplication, QHBoxLayout, QVBoxLayout, QLabel,
+                             QInputDialog, QFileDialog, QMessageBox, QProgressDialog)
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QIcon
 
 formatlist = []
-containerlist =[]
+containerlist = []
 qualitylist = []
 sizelist = []
-downloadOptions=[]
+downloadOptions = []
+
 
 class MainWindow(QWidget):
     systemRun = True
@@ -15,13 +17,14 @@ class MainWindow(QWidget):
     downloadURL = ''
     downloadindex = -1
     saveAddress = 'c:\\'
+
     def __init__(self):
         super().__init__()
         self.Init_UI()
 
     def Init_UI(self):
         self.setWindowTitle('YouTube Downloader made by @Vacabun')
-        self.setMinimumSize(520,400)
+        self.setMinimumSize(520, 400)
         size = self.geometry()
         screen = QDesktopWidget().screenGeometry()
         self.move((screen.width() - size.width()) / 2,
@@ -29,7 +32,7 @@ class MainWindow(QWidget):
         # step1
         self.step1TextLable1 = QLabel("step1:", self)
         self.step1Button = QPushButton('设置代理', self)
-        self.step1TextLable2 = QLabel("代理:"+self.ProxyInfo, self)
+        self.step1TextLable2 = QLabel("代理:" + self.ProxyInfo, self)
         self.step1VBox = QVBoxLayout()
         self.step1VBox.addWidget(self.step1TextLable1)
         self.step1VBox.addWidget(self.step1Button)
@@ -93,7 +96,7 @@ class MainWindow(QWidget):
     def download(self):
         self.step5TextLable2.setText('正在下载中，请稍后。')
         cmd = f'you-get --format={formatlist[self.downloadindex]} -o {self.saveAddress} {self.downloadURL}'
-        #print(cmd)
+        # print(cmd)
         p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         output, errors = p.communicate()
         output = output.decode('UTF-8')
@@ -105,31 +108,31 @@ class MainWindow(QWidget):
         p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         output, errors = p.communicate()
         output = output.decode('UTF-8')
-        pattern = re.compile(r'(- format[^#]*#{1})',re.M)  # 查找数字
+        pattern = re.compile(r'(- format[^#]*#{1})', re.M)  # 查找数字
         result = pattern.findall(output)
         formatlist.clear()
         containerlist.clear()
         qualitylist.clear()
         sizelist.clear()
         for match in result:
-            #解析format
-            format = re.compile(r'(format.*\r\n)',re.M).findall(match)[0]
-            format = re.sub(r'(\r\n)',r'',format)
+            # 解析format
+            format = re.compile(r'(format.*\r\n)', re.M).findall(match)[0]
+            format = re.sub(r'(\r\n)', r'', format)
             format = re.sub(r'format:( )*', r'', str(format))
             formatlist.append(format)
-            #解析container
+            # 解析container
             container = re.compile(r'(container.*\r\n)', re.M).findall(match)[0]
             container = re.sub(r'(\r\n)', r'', container)
             container = re.sub(r'container:( )*', r'', container)
             containerlist.append(container)
-            #解析quality
+            # 解析quality
             quality = re.compile(r'(quality.*\r\n)', re.M).findall(match)[0]
             quality = re.sub(r'(\r\n)', r'', quality)
             quality = re.sub(r'quality:( )*', r'', quality)
             qualitylist.append(quality)
             # 解析size byte
             size = re.compile(r'\(.*\)', re.M).findall(match)[0]
-            size = re.sub(r'[^0123456789]','',size)
+            size = re.sub(r'[^0123456789]', '', size)
             sizelist.append(size)
         self.step5TextLable2.setText('解析下载地址完成。')
 
@@ -138,29 +141,29 @@ class MainWindow(QWidget):
         if sender == self.step1Button:
             text, ok = QInputDialog.getText(self, '设置代理', '请输入代理信息(如127.0.0.1:1080)：')
             if ok:
-                if(text.replace(' ','') == ""):
+                if (text.replace(' ', '') == ""):
                     self.step1TextLable2.setText("无代理" + text)
                 else:
-                    self.step1TextLable2.setText("设置代理:"+text)
+                    self.step1TextLable2.setText("设置代理:" + text)
         elif sender == self.step2Button:
             self.downloadURL, ok = QInputDialog.getText(self, '输入下载链接', '下载链接:')
             if ok:
-                self.step2TextLable2.setText("下载链接:"+self.downloadURL)
-                thread = threading.Thread(target= self.getDoloadInfo)
+                self.step2TextLable2.setText("下载链接:" + self.downloadURL)
+                thread = threading.Thread(target=self.getDoloadInfo)
                 thread.setDaemon(True)
                 thread.start()
 
         elif sender == self.step3Button:
-            if(len(formatlist)):
+            if (len(formatlist)):
                 downloadOptions.clear()
                 for i in range(len(formatlist)):
-                    downloadOptions.append(qualitylist[i]+' '+containerlist[i]+' '+sizelist[i]+'bytes')
+                    downloadOptions.append(qualitylist[i] + ' ' + containerlist[i] + ' ' + sizelist[i] + 'bytes')
                 if len(downloadOptions):
-                    text, ok = QInputDialog.getItem(self, '选择下载格式', '下载格式:',downloadOptions)
+                    text, ok = QInputDialog.getItem(self, '选择下载格式', '下载格式:', downloadOptions)
                     if ok:
                         self.step3TextLable2.setText("下载格式:" + text)
                         self.downloadindex = downloadOptions.index(text)
-                        #print(self.downloadindex)
+                        # print(self.downloadindex)
             else:
                 msgBox = QMessageBox()
                 msgBox.setWindowTitle('错误')
@@ -169,13 +172,14 @@ class MainWindow(QWidget):
                 msgBox.setStandardButtons(QMessageBox.Ok)
                 msgBox.exec()
         elif sender == self.step4Button:
-            self.saveAddress = QFileDialog.getExistingDirectory(self,"选择保存位置","C:/")
+            self.saveAddress = QFileDialog.getExistingDirectory(self, "选择保存位置", "C:/")
             self.step4TextLable2.setText("保存位置:" + self.saveAddress)
         elif sender == self.step5Button:
 
             thread = threading.Thread(target=self.download)
             thread.setDaemon(True)
             thread.start()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
